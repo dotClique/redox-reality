05f010d7a606b1f788e66acc2935bcb6bdca0a78
Mathias Oterhals Myklebust <mathias@oterbust.no>
2023-10-20 15:45:39 +0200

    feat: speech-to-text with Wit.ai and Voice SDK

--- Assets/ChatGPTTest.cs
+++ Assets/ChatGPTTest.cs
@@ -6,48 +6,61 @@ using OpenAI.Managers;
 using OpenAI.ObjectModels;
 using OpenAI.ObjectModels.RequestModels;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChatGPTTest : MonoBehaviour
 {
+    private OpenAIService _openAIService;
+
+    [SerializeField] private Text _debugText;
+    
     // Start is called before the first frame update
     async void Start()
     {
         Debug.Log("Init OpenAI");
-        var openAiService = new OpenAIService(new OpenAiOptions()
+        _openAIService = new OpenAIService(new OpenAiOptions()
         {
             ApiKey = Secrets.OPENAI_API_KEY
         });
-        // TODO: get liquid request from Voice SDK speech-to-text
-        const string liquid = "vinegar";
-        var completionResult = await openAiService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
-        {
-            Messages = new List<ChatMessage>
-            {
-                ChatMessage.FromSystem("You are a helpful assistant. You only answer with single float numbers," +
-                                       " no extra explanations. The number must the parsable, so no characters other" +
-                                       " than digits and decimal separator. If you want to answer with a range," +
-                                       " give the average value of the range"),
-                ChatMessage.FromUser($"What is the pH value of {liquid}?"),
-            },
-            Model = Models.Gpt_3_5_Turbo
-        });
-        if (completionResult.Successful)
-        {
-            Debug.Log(completionResult.Choices.First().Message.Content);
-        }
-        else
-        {
-            if (completionResult.Error == null)
-            {
-                throw new Exception("Unknown Error");
-            }
 
-            Debug.Log($"{completionResult.Error.Code}: {completionResult.Error.Message}");
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
     }
+
+    public void RequestResponse(string query)
+    {
+        Debug.Log($"Requesting response with query: {query}");
+        // // TODO: get liquid request from Voice SDK speech-to-text
+        // var completionResult = await _openAIService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
+        // {
+        //     Messages = new List<ChatMessage>
+        //     {
+        //         ChatMessage.FromSystem("You are a helpful assistant. You only answer with single float numbers," +
+        //                                " no extra explanations. The number must the parsable, so no characters other" +
+        //                                " than digits and decimal separator. If you want to answer with a range," +
+        //                                " give the average value of the range"),
+        //         ChatMessage.FromUser($"What is the pH value of {query}?"),
+        //     },
+        //     Model = Models.Gpt_3_5_Turbo
+        // });
+        // if (completionResult.Successful)
+        // {
+        //     _debugText.text = completionResult.Choices.First().Message.Content;
+        // }
+        // else
+        // {
+        //     if (completionResult.Error == null)
+        //     {
+        //         throw new Exception("Unknown Error");
+        //     }
+        //
+        //     Debug.Log($"{completionResult.Error.Code}: {completionResult.Error.Message}");
+        // }
+    }
+    
+    
+    
 }
