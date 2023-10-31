3ecec12aed545390e2e187c07ce47840c7e48e17
Lars Mitsem Selbekk <lars@selbekk.com>
2023-10-31 15:27:12 +0100

    feat: add microphone activation styling

--- Assets/ActivateVoice.cs
+++ Assets/ActivateVoice.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6b582e81c326a10122f4a278276075948b767c905124b296fd7a59ef3738036
-size 605
+oid sha256:68f3e4e895b5487b4af4d47e2b681c433ca82b4aae2c5bdd5c02bc4ca8f5ac19
+size 674
---
+++ Assets/MicrophoneActivator.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9bd53a007d2a231844e3ae5455c287adce0a6df6b1860c2d68582a5a40830144
+size 591
