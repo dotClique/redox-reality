b5d7437ff222e1ada113289a3208e7aa8db99662
Mathias Oterhals Myklebust <mathias@oterbust.no>
2023-11-09 19:39:40 +0100

    feat: use LabAI ph value in spawned beaker liquid

--- Assets/LabAI.cs
+++ Assets/LabAI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fcff79dd7f42b01a9b30e55ba566222061711122753503f044ccc1cedfe5f81
-size 6310
+oid sha256:39a20e783f0bef7e748f9715166687d9b44a34e4cf639ce63696d46891d07e27
+size 6557
