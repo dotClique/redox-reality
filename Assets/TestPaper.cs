b0259182c65addf3b8f9d561b839f508531aed0e
Lars Mitsem Selbekk <lars@selbekk.com>
2023-11-19 20:29:14 +0100

    feat: gradually change pH-color

--- Assets/TestPaper.cs
+++ Assets/TestPaper.cs
@@ -1,11 +1,16 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TestPaper : MonoBehaviour
 {
     private Renderer _renderer;
-    
-    private static readonly Color32[] PhBaseColors = {
+    private int _lerpCount;
+    private Color _destinationColor = Color.white, _startColor = Color.white;
+    [Serialize] public int lerpTime = 1000;
+
+    private static readonly Color32[] PhBaseColors =
+    {
         new(238, 55, 34, 255), // 1
         new(238, 52, 121, 255), // 2
         new(245, 126, 38, 255), // 3
@@ -21,6 +26,8 @@ public class TestPaper : MonoBehaviour
         new(148, 36, 140, 255), // 13
         new(123, 39, 121, 255), // 14
     };
+
+    private static readonly int BaseColor = Shader.PropertyToID("_Color");
 
     private static Color pHToColor(float pH)
     {
@@ -45,14 +52,19 @@ public class TestPaper : MonoBehaviour
 
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         _renderer = GetComponent<Renderer>();
+        _lerpCount = lerpTime + 1;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (_lerpCount++ <= lerpTime)
+        {
+            _renderer.material.SetColor(BaseColor, GetColor());
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -63,8 +75,17 @@ public class TestPaper : MonoBehaviour
             var liquid = collision.gameObject.GetComponent<Liquid>();
             Debug.Log($"Liquid has ph {liquid.pH}");
             Debug.Log($"That would be the color {pHToColor(liquid.pH)}");
-            _renderer.material.SetColor("_Color", pHToColor(liquid.pH));
+            _startColor = GetColor();
+            _destinationColor = pHToColor(liquid.pH);
+            _lerpCount = 0;
+
             GameManager.Instance.SetSecondaryPanelText("use the microphone to submit your pH guess");
         }
     }
+
+    private Color GetColor()
+    {
+        return Color.Lerp(_startColor, _destinationColor, 1 - Mathf.Pow(1 - (float)_lerpCount / lerpTime, 3));
+    }
+
 }
